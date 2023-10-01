fc50dd1a92d4b256c5cfa86f99008ca2ae83f4aa
Mounir Hamza <hamza@qover.com>
2023-10-01 02:34:24 +0200

    feat: add blog part

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import Button from './button'
+import { InvertedButton } from './button'
 
 export default function Header() {
   return (
     <nav
       className={
-        'z-50 sticky top-0 mx-auto overflow-auto transition-all duration-300 ease-in-ou text-white bg-primary-700'
+        'z-50 sticky top-0 mx-auto overflow-auto transition-all duration-300 ease-in-ou text-black shadow-sm bg-white'
       }
     >
       <div className={`p-4 w-full max-w-screen-xl mx-auto`}>
         <div className='font-bold leading-normal w-full flex flex-row gap-2 items-center justify-between'>
           <Link href={'/'}>
-            <Image loading='lazy' width={50} height={60} src='/favicon2.png' alt={`Logo`} />
+            <Image loading='lazy' width={50} height={60} src='/logo.png' alt={`Logo`} />
           </Link>
-          <Button message='Contact' link='/contact' />
+          <InvertedButton message='Contact' link='/contact' />
         </div>
       </div>
     </nav>
---
+++ src/pages/blog.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+import Layout from '../components/layout'
+import { useTranslate } from '../hooks/useTranslate'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { allArticles, filters } from '../api/articles'
+import Container from '../components/container'
+import ArticleCard from '../components/card/article'
+
+export default function Blog() {
+  const router = useRouter()
+  const { t } = useTranslate()
+  const [articles, setArticles] = useState(allArticles)
+  const [filter, setFilter] = useState(filters[0])
+
+  useEffect(() => {
+    if (router.query.filter?.length > 0) {
+      setFilter(router.query.filter)
+      const filter = allArticles.find((item) => item.topic === router.query.filter)
+      if (router.query.filter === filters[0]) {
+        return setArticles(allArticles)
+      }
+      setArticles(allArticles.filter((item) => item.topic === filter?.topic) || [])
+    }
+  }, [router.query])
+
+  return (
+    <Layout>
+      <Container>
+        <div className='flex flex-col items-start justify-between  gap-4 w-full'>
+          <div className='text-2xl xl:text-4xl font-bold'>{t('articles.title')}</div>
+          <div className='text-base font-normal max-w-2xl'>{t('articles.description')}</div>
+          <div className='flex flex-col md:flex-row text-primary-500'>
+            {filters.map((item, index) => (
+              <Link key={index} href={`/blog?filter=${item}`}>
+                <div
+                  className={`transition-all cursor-pointer text-base pr-5 mb-5 text-black md:m-0 ${
+                    item === filter && 'font-bold underline underline-offset-8 text-primary-500'
+                  } hover:font-bold hover:underline-offset-8 hover:underline`}
+                >
+                  {t(item)}
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        <div className='items-center w-full'>
+          <div className='grid grid-flow-dense grid-cols-1 md:grid-cols-3 gap-4'>
+            {articles.map((article) => (
+              <ArticleCard article={article} key={article.name} />
+            ))}
+          </div>
+        </div>
+      </Container>
+    </Layout>
+  )
+}

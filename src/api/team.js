2268efc0bec2a69fd07d7ec209ea86a6e5439b79
Hamza mounir <hamza.pixelle@gmail.com>
2023-10-19 20:41:56 +0200

    feat: add team memabers

--- src/api/team.js
+++ src/api/team.js
@@ -59,4 +59,44 @@ export const team = [
       },
     ],
   },
+  {
+    name: 'team.4.name',
+    role: 'team.4.role',
+    description: 'team.4.description',
+    avatar: '/images/team/arno.png',
+    socials: [
+      {
+        title: 'facebook',
+        link: 'https://www.facebook.com/clevertech',
+      },
+      {
+        title: 'instagram',
+        link: 'https://www.instagram.com/',
+      },
+      {
+        title: 'linkedin',
+        link: 'https://www.linkedin.com/clevertech',
+      },
+    ],
+  },
+  {
+    name: 'team.5.name',
+    role: 'team.5.role',
+    description: 'team.5.description',
+    avatar: '/images/team/killian.png',
+    socials: [
+      {
+        title: 'facebook',
+        link: 'https://www.facebook.com/clevertech',
+      },
+      {
+        title: 'instagram',
+        link: 'https://www.instagram.com/',
+      },
+      {
+        title: 'linkedin',
+        link: 'https://www.linkedin.com/clevertech',
+      },
+    ],
+  },
 ]
